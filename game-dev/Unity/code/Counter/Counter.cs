34b63e39b806553e8530610819b6b84384611f4e
tom-choi <58120532+tom-choi@users.noreply.github.com>
2022-11-27 19:19:21 +0800

    Create Counter.cs

---
+++ game-dev/Unity/code/Counter/Counter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public Text CounterText;
+
+    private int Count = 0;
+
+    private void Start()
+    {
+        Count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Count += 1;
+        CounterText.text = "Count : " + Count;
+    }
+}
