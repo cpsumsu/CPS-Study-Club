d8fd39bc0c2ac9e5ac8fa1d59d3490f7031fb2a5
awepo-pro <dc22762@umac.mo>
2023-08-24 07:46:39 +0800

    visibility of namespace with using

---
+++ programming-lang/C++ feature/namespace/hidden namespace member.cpp
@@ -0,0 +1,24 @@
+// since A::b and b are not in the same scope, so local variable b hide namespace member name,
+// so need to call namespace member b explicitly
+#include <iostream>
+using namespace std;
+
+namespace A {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+}
+
+int a = 40;             // global scope
+
+int main() {
+    
+    using namespace A;
+    cout << b << endl;      // A::b = 20 shown
+
+    int b = 50;
+    cout << b << endl;      // b = 50 shown, which means it hide A::b
+    cout << A::b << endl;
+
+    return 0;
+}
